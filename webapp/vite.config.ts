a73f44033abefa4a71f06bef1933c63b002ab968
tonitrnel <tonitrnel@outlook.com>
2023-06-04 15:46:02 +0800

    chore: add alias resolve.

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import type { CompilerOptions } from 'typescript';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 import wasm from 'vite-plugin-wasm';
@@ -6,18 +7,59 @@ import toml from 'toml';
 import fs from 'node:fs/promises';
 import path from 'node:path';
 
-// https://vitejs.dev/config/
-export default defineConfig(async () => {
+const __ROOT = process.cwd();
+const parseTSAlias = async () => {
+  try {
+    const { baseUrl = '.', paths = {} }: CompilerOptions = await fs
+      .readFile(path.join(__ROOT, '/tsconfig.json'))
+      .then((r) => {
+        const content = r
+          .toString()
+          .replace(/^\s+\/\*.+\*\/\s*$/gm, '')
+          .replace(/\/\/.*$/gm, '');
+        return JSON.parse(content).compilerOptions;
+      });
+    return Object.keys(paths).reduce((alias, key) => {
+      alias[key.toString().replace('/*', '')] = path.join(
+        __ROOT,
+        baseUrl,
+        paths[key][0]?.replace('/*', '')
+      );
+      return alias;
+    }, {} as Record<string, string>);
+  } catch (e) {
+    console.error('[Vite] Parse tsconfig failed, return empty alias', e);
+    return {};
+  }
+};
+const injectEnvironmentVariables = async (): Promise<void> => {
   const config = toml.parse(
     await fs
-      .readFile(path.join(process.cwd(), '../synclink-config.toml'))
+      .readFile(path.join(__ROOT, '../synclink-config.toml'))
       .then((r) => r.toString())
-  );
+  ) as {
+    server: {
+      host: string;
+      port: number;
+    };
+    https?: {
+      port: number;
+    };
+  };
+
   process.env['VITE_APP_ENDPOINT'] = config.https
     ? `https://${config.server.host}:${config.https.port}/api`
     : `http://${config.server.host}:${config.server.port}/api`;
+};
 
+// https://vitejs.dev/config/
+export default defineConfig(async () => {
+  await injectEnvironmentVariables();
+  console.log(await parseTSAlias());
   return {
     plugins: [react(), svgr(), wasm()],
+    resolve: {
+      alias: await parseTSAlias(),
+    },
   };
 });
