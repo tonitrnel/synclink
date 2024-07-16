a260c33c456955939aa2fe2559e5b9019a462f1c
tonitrnel <tonitrnel@outlook.com>
2024-07-16 21:18:13 +0800

    fixed: parse the 'quota' field in the configuration file chore: remove the 'max_size_of' field from the configuration file

--- server/src/config/storage_config.rs
+++ server/src/config/storage_config.rs
@@ -1,13 +1,13 @@
 use crate::config::root_dir;
 use anyhow::Context;
-use serde::Deserialize;
+use serde::{Deserialize, Deserializer};
 use std::path::PathBuf;
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct StorageConfig {
     pub storage_path: String,
+    #[serde(deserialize_with = "size_deserialize", default)]
     pub quota: Option<usize>,
-    pub max_size_of: Option<usize>,
 }
 
 impl StorageConfig {
@@ -20,5 +20,27 @@ impl StorageConfig {
         };
         path.canonicalize()
             .with_context(|| "Failed to parse storage directory.".to_string())
+    }
+}
+
+fn size_deserialize<'de, D>(deserializer: D) -> Result<Option<usize>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s: Option<String> = Deserialize::deserialize(deserializer)?;
+    let s = if let Some(s) = s {
+        s.to_lowercase()
+    } else {
+        return Ok(None);
+    };
+    let (number, unit) = s.split_at(s.len() - 2);
+    let number = number
+        .parse::<usize>()
+        .map_err(|_| serde::de::Error::custom(format!("Invalid digest: {}", number)))?;
+    match unit {
+        "kb" => Ok(Some(number * 1024)),
+        "mb" => Ok(Some(number * 1024 * 1024)),
+        "gb" => Ok(Some(number * 1024 * 1024 * 1024)),
+        _ => Err(serde::de::Error::custom(format!("Invalid unit: {}", unit))),
     }
 }
