d5b7b5175822f64ced94223570581dc98265ae8d
tonitrnel <tonitrnel@outlook.com>
2025-05-10 16:11:46 +0800

    chore(web): allow `event-bus` to receive return values.

--- web/src/utils/event-bus.ts
+++ web/src/utils/event-bus.ts
@@ -1,45 +1,79 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+type EventParameter<T> = T extends [infer U, unknown] ? U : T;
+type EventReturnType<T> = T extends [unknown, infer U] ? U : void;
+type UnknownEventFn = (value: unknown) => unknown;
+type EmitReturnType<T> = T extends Promise<infer U> ? Promise<U[]> : T[];
 
 export class EventBus<
-    EventMap extends Record<string, any>,
+    EventMap extends Record<
+        string,
+        unknown | [input: unknown, output: unknown]
+    >,
     T extends keyof EventMap = keyof EventMap,
 > {
-    private pools: Map<T, Set<(value: any) => void>>;
+    private pools: Map<T, Set<(value: unknown) => unknown>>;
+
     constructor() {
         this.pools = new Map();
     }
-    public on<K extends T>(event: K, listener: (value: EventMap[K]) => void) {
+
+    public on<K extends T>(
+        event: K,
+        listener: (
+            value: EventParameter<EventMap[K]>,
+        ) => EventReturnType<EventMap[K]>,
+    ) {
         if (!this.pools.has(event)) {
             this.pools.set(event, new Set());
         }
-        this.pools.get(event)!.add(listener);
+        this.pools.get(event)!.add(listener as UnknownEventFn);
         return () => this.off(event, listener);
     }
-    public off<K extends T>(event: K, listener: (value: EventMap[K]) => void) {
-        this.pools.get(event)?.delete(listener);
+
+    public off<K extends T>(
+        event: K,
+        listener: (
+            value: EventParameter<EventMap[K]>,
+        ) => EventReturnType<EventMap[K]>,
+    ) {
+        this.pools.get(event)?.delete(listener as UnknownEventFn);
     }
+
     public clear(event: T) {
         this.pools.delete(event);
     }
-    public once<K extends T>(event: K, listener: (value: EventMap[K]) => void) {
-        const off = this.on(event, (v: EventMap[K]) => {
-            listener(v);
+
+    public once<K extends T>(
+        event: K,
+        listener: (
+            value: EventParameter<EventMap[K]>,
+        ) => EventReturnType<EventMap[K]>,
+    ) {
+        const off = this.on(event, (v: EventParameter<EventMap[K]>) => {
+            const ret = listener(v);
             off();
+            return ret;
         });
         return off;
     }
+
     public emit<K extends T>(
         ...[event, value]: EventMap[K] extends undefined
             ? [event: K]
-            : [event: K, value: EventMap[K]]
-    ) {
+            : [event: K, value: EventParameter<EventMap[K]>]
+    ): EmitReturnType<EventReturnType<EventMap[K]>> | void {
         if (!this.pools.has(event)) return void 0;
-        return Promise.all(
-            [...this.pools.get(event)!.values()].map((fn) =>
-                fn(value as EventMap[K]),
-            ),
+        const returns = [...this.pools.get(event)!.values()].map((fn) =>
+            fn(value as EventMap[K]),
         );
+        if (returns.some((it) => it instanceof Promise)) {
+            return Promise.all(returns) as EmitReturnType<
+                EventReturnType<EventMap[K]>
+            >;
+        } else {
+            return returns as EmitReturnType<EventReturnType<EventMap[K]>>;
+        }
     }
+
     public batch(...unregisters: Array<() => void>) {
         return () => unregisters.forEach((unregister) => unregister());
     }
